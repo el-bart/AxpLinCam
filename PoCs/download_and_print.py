e4d00b9b3bd9f105b07c3767c5652a2f16ad7a21
Bartek Szurgot <bartek.szurgot@baszerr.eu>
2013-02-15 22:04:05 +0100

    added PoCs for http

---
+++ PoCs/download_and_print.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+import sys
+import requests
+
+if len(sys.argv)==2:
+    link=sys.argv[1]
+else:
+    link="http://www.baszerr.eu"
+
+r = requests.get(link)
+
+#print( r.text.encode("utf-8") )
+#sys.exit(0)
+
+print("response: " + str(r.status_code))
+r.raise_for_status()
+print("headers: " + str(r.headers))
+print( "text: " + r.text.encode("utf-8") )
