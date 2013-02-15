c7b33256db4fd22a55c118e6fa793777248b348f
Bartek Szurgot <bartek.szurgot@baszerr.eu>
2013-02-15 22:35:28 +0100

    minor refactoring; error checking fixed; comments added

--- src/AxpLinCam.py
+++ src/AxpLinCam.py
@@ -3,32 +3,44 @@
 import requests
 
 
+# main class for managing the camera device
 class Camera:
+    # stores all the required parameters
     def __init__(self, host, user, password):
         self.__host = host
         # NOTE: currently user name name and password are not used (required only for streaming)
         self.__user = user
         self.__pass = password
 
+    # moves
     def up(self, multi=False):
-        self.__move("up",   0 if multi else 1)
+        self.__action("up",   0 if multi else 1)
     def down(self, multi=False):
-        self.__move("down", 0 if multi else 1)
+        self.__action("down", 0 if multi else 1)
     def left(self, multi=False):
-        self.__move("left", 0 if multi else 1)
+        self.__action("left", 0 if multi else 1)
     def right(self, multi=False):
-        self.__move("right", 0 if multi else 1)
+        self.__action("right", 0 if multi else 1)
 
+    # misc commands
     def home(self):
-        self.__move("home", 0)
+        self.__action("home", None)
     def stop(self):
-        self.__move("stop", 0)
+        self.__action("stop", None)
 
-    def __move(self, act, step):
-        link = "http://" + self.__host + "/web/cgi-bin/hi3510/ptzctrl.cgi?-act=" + act + "&-step=" + str(step)
+
+    # actual implementation
+    def __action(self, act, step):
+        # basic link with the command
+        link = "http://" + self.__host + "/web/cgi-bin/hi3510/ptzctrl.cgi?-act=" + act
+        # is direct move command?
+        if step is not None:
+            link += "&-step=" + str(step)
+        # send the request
         r = requests.get(link)
-        if r.status_code != 100:
-            r.raise_for_status()
-        #r.text.encode("utf-8")
-
-
+        # ensure it didn't failed
+        r.raise_for_status()
+        # check the response
+        txt = ' '.join( r.text.splitlines() ).encode("utf-8")
+        if txt != '[Success]call ptz funtion ok':
+            raise Exception( "invalid/unknown reponse from '" + self.__host + "': " + txt )
