14a6a13ee2146c8a930d447f3a1eea9508e39b7c
Bartek Szurgot <bartek.szurgot@baszerr.eu>
2013-02-15 22:10:25 +0100

    moving camera is now fully working

--- src/AxpLinCam.py
+++ src/AxpLinCam.py
@@ -1,34 +1,34 @@
 #!/usr/bin/python
 
+import requests
+
 
 class Camera:
     def __init__(self, host, user, password):
         self.__host = host
+        # NOTE: currently user name name and password are not used (required only for streaming)
         self.__user = user
         self.__pass = password
 
     def up(self, multi=False):
-        print "up"
-        return True
-
+        self.__move("up",   0 if multi else 1)
     def down(self, multi=False):
-        print "down"
-        return True
-
+        self.__move("down", 0 if multi else 1)
     def left(self, multi=False):
-        print "left"
-        return True
-
+        self.__move("left", 0 if multi else 1)
     def right(self, multi=False):
-        print "right"
-        return True
+        self.__move("right", 0 if multi else 1)
 
     def home(self):
-        print "home"
-        return True
-
+        self.__move("home", 0)
     def stop(self):
-        print "stop"
-        return True
+        self.__move("stop", 0)
+
+    def __move(self, act, step):
+        link = "http://" + self.__host + "/web/cgi-bin/hi3510/ptzctrl.cgi?-act=" + act + "&-step=" + str(step)
+        r = requests.get(link)
+        if r.status_code != 100:
+            r.raise_for_status()
+        #r.text.encode("utf-8")
 
 
